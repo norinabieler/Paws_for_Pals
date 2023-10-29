3ad890abb72e21fd127b85179c420c12007cb58d
norinabieler <norina.bieler@stud.fhgr.ch>
2023-10-29 16:20:03 +0100

    Update script_kundenprofil.js

--- scripts/script_kundenprofil.js
+++ scripts/script_kundenprofil.js
@@ -8,3 +8,18 @@ document.getElementById('feed-button').addEventListener('click', () => {
 document.getElementById('profile-button').addEventListener('click', () => {
     window.location.href = 'kundenprofil.html';
 });
+
+
+// FUNKTIONIERT SO NOCH NICHT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        // Funktion zum Löschen eines Listenelements
+        function deleteListItem(event) {
+            const listItem = event.target.parentElement; // Das übergeordnete Listenelement
+            listItem.remove(); // Entfernen Sie das Listenelement aus dem DOM
+        }
+
+        // Alle Löschen-Buttons auswählen und Event-Handler hinzufügen
+        const deleteButtons = document.querySelectorAll(".deleteButton");
+        deleteButtons.forEach(button => {
+            button.addEventListener("click", deleteListItem);
+        });
