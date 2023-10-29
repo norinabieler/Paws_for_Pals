d075290d87cf5d59ae8b13da26c7a3559a199fa2
norinabieler <norina.bieler@stud.fhgr.ch>
2023-10-29 16:20:01 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,35 @@
 import { supa } from "../config/config.js";
 
+// HIER FUNKTIONIERT ALLES NOCH NICHT SO RICHTIG
+
+// Function to login using email and password
+async function login() {
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passwordInput').value;
+
+    const { error } = await supa.auth.signIn({ email, password });
+
+    if (error) {
+        console.error("Error during login: ", error.message);
+    } else {
+        console.log("Logged in as ", email);
+    }
+}
+
+document.getElementById('loginButton').addEventListener('click', () => {
+    window.location.href = 'feed.html';
+});
+
+// Function to sign up using email and password
+async function signUp() {
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passwordInput').value;
+
+    const { error } = await supa.auth.signUp({ email, password });
+
+    if (error) {
+        console.error("Error during sign up: ", error.message);
+    } else {
+        console.log("Signed up as ", email);
+    }
+}
