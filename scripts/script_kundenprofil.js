d7d6adfa30a0ddd28bf8728e432d77897ea84fe7
norinabieler <norina.bieler@stud.fhgr.ch>
2023-10-29 14:08:41 +0100

    Update script_kundenprofil.js

--- scripts/script_kundenprofil.js
+++ scripts/script_kundenprofil.js
@@ -1,2 +1,5 @@
 import { supa } from "../config/config.js";
 
+// Hier dann noch richtig mit Unterseiten verknüpfen
+document.getElementById('feed-button').addEventListener('click', () => alert('Zum Feed gehen'));
+document.getElementById('profile-button').addEventListener('click', () => alert('Zum Profil gehen'));
