a7043cc11126f006df5ee4b237ab53fdd02c44ef
norinabieler <norina.bieler@stud.fhgr.ch>
2023-11-08 22:01:58 +0100

    Update script_login.js

--- scripts/script_login.js
+++ scripts/script_login.js
@@ -3,9 +3,23 @@ import { supa } from "../config/config.js";
 console.log("Script.js ausgeführt");
 
 
-// HIER FUNKTIONIERT ALLES NOCH NICHT SO RICHTIG
+// Function to check if the user is logged in and redirect if necessary
+async function checkUser() {
+    const user = supa.auth.user();
+    if (user) {
+        console.log("User is already logged in as", user.email);
+        window.location.href = 'http://localhost:5500/index.html'; // Weiterleitung zu einer anderen Seite
+    } else {
+        console.log("User is not logged in.");
+    }
+}
 
-// Function to login using email and password
+// Event listener für das Überprüfen des Benutzers beim Laden der Seite
+window.addEventListener('load', checkUser);
+
+
+
+// Function to login using email and password_______________________________________________________________________________________________
 async function login() {
     console.log("Login ausgeführt");
     const email = document.getElementById('emailInput').value;
@@ -20,8 +34,11 @@ async function login() {
         window.location.href = 'http://localhost:5500/index.html'
 
     }
-
 }
+
+//______________________________________________________________________________________________________________________________
+
+
 
 // Function to sign up using email and password
 async function signUp() {
@@ -37,6 +54,10 @@ async function signUp() {
     }
 }
 
+//________________________________________________________________________________________________________________________________________________
+
+
+
  // Function to update user status
 function updateUserStatus(user) {
     const userStatusElement = document.getElementById('userStatus');
@@ -48,13 +69,12 @@ function updateUserStatus(user) {
     }
 } 
 
-
+//____________________________________________________________________________________________________________________________________
 
 // Check and display the initial user status
 const initialUser = supa.auth.user();
 updateUserStatus(initialUser);
-/* updateUserStatus(initialUser);
- */
+
 // Event listeners for the buttons
 document.getElementById('loginButton').addEventListener('click', login);
 
