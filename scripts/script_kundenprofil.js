e5362871c85b19dbffd5f0e83e45477b6f656553
norinabieler <norina.bieler@stud.fhgr.ch>
2023-11-08 20:13:54 +0100

    Update script_kundenprofil.js

--- scripts/script_kundenprofil.js
+++ scripts/script_kundenprofil.js
@@ -19,7 +19,7 @@ console.log(initialUser.id);
 
 
 
-
+fetchAndAppendAnimalData();
 
 
 
@@ -32,66 +32,71 @@ document.getElementById('profile-button').addEventListener('click', () => {
 });
 
 
-// Funktion zum Abmelden
-async function logout() {
-
-    //Add console log if function works
-    console.log("Logout ausgeführt");
-
-
-    const { error } = await supa.auth.signOut();
-  
-    if (!error) {
-      alert("Erfolgreich abgemeldet.");
-      window.location.href = "login.html"; // Optional: Nach der Abmeldung zur Login-Seite weiterleiten
-    } else {
-      alert("Fehler beim Abmelden: " + error.message);
-    }
-  }
-  
-  // Event-Listener für den Abmelde-Button
-  const logoutButton = document.getElementById("logoutButton");
-  if (logoutButton) {
-    logoutButton.addEventListener("click", logout);
-  }
 
 
 // Hier Funktion um Patentiere einzufügen 
 // Abrufen und Einbinden von Daten aus Supabase
 
-async function fetchAndAppendFeedData() {
+async function fetchAndAppendAnimalData() {
+  const patentierContainer = document.getElementById('patentiere');
+  patentierContainer.innerHTML = '';
+
+  const userId = initialUser.id
+
   const { data, error } = await supa
     .from('Animals') // Hier 'deine_tabelle' durch den Namen deiner Tabelle ersetzen
-    .select('Name, Tierart, Preis, Herkunft, Kategorie_ID(Kontinent), Alter, Geschlecht, id')
+    .select('Name, Tierart, Preis, Picture, Herkunft, Kategorie_ID(Kontinent), Alter, Geschlecht, Beschreibung, id, User_Id')
+    .eq('User_Id', userId);
 
-    console.log(data)
+  console.log(data)
 
   if (error) {
     console.error('Fehler beim Abrufen der Daten:', error);
     return;
   }
 
-  const patentierContainer = document.getElementById('patentiere'); 
-
   // Daten aus der Supabase-Tabelle in deinen Feed einfügen
   data.forEach(tier => {
     
      let output = `
-      <div id="patentiere">
+      <div class="Box">
+          <div class="text">
             <h3>${tier.Name}</h3>
             <p>${tier.Tierart}, ${tier.Preis} CHF/Monat</p>
+          </div>
             <p><b>Herkunft:</b> ${tier.Herkunft}, ${tier.Kategorie_ID.Kontinent}</p>
             <p><b>Alter:</b> ${tier.Alter}</p>
             <p><b>Geschlecht:</b> ${tier.Geschlecht}</p>
+            <button id="deleteButton-${tier.id}" class="delete-button>Spende beenden</button>
       </div>
     `;
-
     patentierContainer.innerHTML += output;
   });
 }
 
 
+// Funktion zum Abmelden
+async function logout() {
 
+  //Add console log if function works
+  console.log("Logout ausgeführt");
+
+
+  const { error } = await supa.auth.signOut();
+
+  if (!error) {
+    alert("Erfolgreich abgemeldet.");
+    window.location.href = "login.html"; // Optional: Nach der Abmeldung zur Login-Seite weiterleiten
+  } else {
+    alert("Fehler beim Abmelden: " + error.message);
+  }
+}
+
+// Event-Listener für den Abmelde-Button
+const logoutButton = document.getElementById("logoutButton");
+if (logoutButton) {
+  logoutButton.addEventListener("click", logout);
+}
 
 
 /* // FUNKTIONIERT SO NOCH NICHT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
